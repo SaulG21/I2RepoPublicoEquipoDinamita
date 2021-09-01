156c48f0bc9eed68607765282b1c50dc2e45f478
SaulSG21 <2183042802@cua.uam.mx>
2021-08-31 21:53:55 -0500

    Se crearon biblioteca cadena.dll y cabecera cadena.h.

--- lib/include/cadena.h
+++ lib/include/cadena.h
@@ -1,5 +1,5 @@
 /**
- * @file lib.h
+ * @file cadena.h
  * @authors: 
  *          1. Garcia Godoy Victor Saul
  *          2. Alcantara Segura Karina
@@ -7,8 +7,8 @@
  *          4. Mercado Fernández Israel Yosafat
  *          5. Benitez Garcia Alan David
  * @brief  This library is based on string.h.
- *         It allow operate with arrangements of characters, for example it can 
- *         get the size of one string or compare two strings, among other operations
+ *         It allow operate with arrangements of characters (strings), for example it can 
+ *         get the size of one string, or compare two strings, among other operations
  *         presented here.      
  * @version 0.1
  * @date 2021-08-31
@@ -31,7 +31,7 @@ char *comparar(const char *s1, const char *s2);
  * @param char *cad
  * @return chain size 
  */
-int cadinv(char *cad); //hay que modificar este nombre
+int cuentacadena(char *cad);
 
 /**
  * @brief This function takes an array of integers and the size of 
