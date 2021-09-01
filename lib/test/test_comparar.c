caba18cdf06f4af216045be8485de18c1a185ced
KaryAlc <2203025452@cua.uam.mx>
2021-09-01 16:37:41 -0500

    Cambio de funcion "comparar"

--- lib/test/test_comparar.c
+++ lib/test/test_comparar.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-#include "..\src\comparar.c"     
+#include<string.h>
+#include<stdio.h>
+#include "..\src\comparar.c" 
 
-int main(){
-
-    const char *s1, *s2;
-    char cadena[50], cadena2 [50];
-    printf("Introduce la cadena 1: ");
-    gets(cadena);
-    printf("Introduce la cadena 2: ");
-    gets(cadena2);
-    s1 = cadena;
-    s2 = cadena2;
-    printf(comparar(s1,s2));
+int main (){
+    char cadena1[] = "ASDFASDFASDFASDF";
+    char cadena2[] = "hola";
+    comparar(cadena1,cadena2);
+    
     return 0;
 }
